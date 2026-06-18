39eb73b21ae02fa43dd10c3b02d0b6405209c3ae
Natali <Natalikristal@yandex.ru>
2026-06-18 09:24:57 +0300

    Подключила репортер jest-html-reporter

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  // Транслируем все файлы через Babel
-  transform: {
-    '^.+\\.[jt]sx?$': ['babel-jest', { presets: ['@babel/preset-env'] }]
-  },
-
   // Разрешили Babel заходить в node_modules
   transformIgnorePatterns: [
     '/node_modules/(?!@babel|.*\\.mjs$)', // Для Babel
@@ -12,4 +7,22 @@ module.exports = {
 
   // Указываем, где искать тесты
   testMatch: ['**/tests/**/*.test.js'],
+
+  // Транслируем все файлы через Babel
+  transform: {
+    '^.+\\.[jt]sx?$': ['babel-jest', { presets: ['@babel/preset-env'] }]
+  },
+
+  // Добавляем reporters для генерации HTML-отчета
+  reporters: [
+    'default',
+    [
+      'jest-html-reporters',
+      {
+        publicPath: './reports/html-report',
+        filename: 'index.html',
+        openReport: true //!process.env.CI
+      }
+    ]
+  ],
 };
