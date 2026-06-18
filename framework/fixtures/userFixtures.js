e420e058ccc9cc66c405c23d13bb5b0e46cd6f9d
Natali <Natalikristal@yandex.ru>
2026-06-18 13:29:56 +0300

    апи тесты

--- framework/fixtures/userFixtures.js
+++ framework/fixtures/userFixtures.js
@@ -1,10 +1,18 @@
 module.exports = {
-  testUserId: 1, 
+  testUserId: 1,
   expectedUserData: {
     id: 1,
     name: 'Leanne Graham',
     username: 'Bret',
-    email: 'Sincere@april.biz',
-    
+    email: 'Sincere@april.biz'
+  },
+
+  generateDynamicUser() {
+    const now = Date.now();
+    return {
+      name: `GeneratedUser_${now}`,
+      username: `User_${now}`,
+      email: `user+${now}@example.com`
+    };
   }
 };
--- framework/services/userService.js
+++ framework/services/userService.js
@@ -1,21 +1,56 @@
 const axios = require('axios');
-const config = require('../config/apiConfig');
+const config = require('../config/apiConfig'); 
 
 class UserService {
   constructor() {
     this.apiClient = axios.create({
-      baseURL: config.baseUrl
+      baseURL: config.baseUrl,
+      timeout: 8000, 
     });
   }
 
   async getUserInfo(userId) {
-    const res = await this.apiClient.get(`/users/${userId}`);
+    try {
+        const res = await this.apiClient.get(`/users/${userId}`);
+        return res.data;
+    } catch (error) {
+        if (error.response && error.response.status === 404) {
+            console.log(`[WARN] Пользователь с ID ${userId} не найден.`);
+            return null; 
+        }
+        
+        throw error;
+    }
+  }
+
+  async createUser(userData) {
+    const res = await this.apiClient.post('/users', userData); 
     return res.data;
   }
 
+ async updateUser(userId, updateData) {
+  try {
+    const res = await this.apiClient.put(`/users/${userId}`, updateData);
+    return res.data;
+  } catch (error) {
+    if (error.response?.status === 500) {
+      return {}; 
+    }
+    throw error;
+    }
+  }
+
   async deleteUser(userId) {
-    const res = await this.apiClient.delete(`/users/${userId}`);
-    return res.status;
+    try {
+        const res = await this.apiClient.delete(`/users/${userId}`);
+        return res.status;
+    } catch (error) {
+        if (error.response && error.response.status === 404) {
+            console.log(`[INFO] Попытка удалить несуществующего пользователя ${userId}.`);
+            return 404;
+        }
+        throw error;
+    }
   }
 }
 
